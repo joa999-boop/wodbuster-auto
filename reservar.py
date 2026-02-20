4b5e1d138bb5a91221391dfb59af3d47367320c3
joa999-boop <joa_megane@hotmail.com>
2026-02-20 21:41:55 +0100

    Create reservar.py

---
+++ reservar.py
@@ -0,0 +1,22 @@
+import os
+from playwright.sync_api import sync_playwright
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+
+    # URL de login de WodBuster
+    page.goto("https://app.wodbuster.com/login")
+
+    page.fill("input[name='email']", USERNAME)
+    page.fill("input[name='password']", PASSWORD)
+    page.click("button[type='submit']")
+
+    page.wait_for_timeout(5000)
+
+    print("Login realizado correctamente")
+
+    browser.close()
